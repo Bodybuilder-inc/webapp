cab95e0fadd0444afdce5f4fdaed7ec0fa193975
samueljrz <samuelevangelista.ti@gmail.com>
2021-10-23 13:51:03 -0300

    setup test 2

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <div>Vai ser aqui o cadstro</div>
+  return <div>Vai ser aqui o cadastro</div>
 }
 
 export default Home

c57f3e6a249c0dd4eaa550c682cccdf56e208b62
samueljrz <samuelevangelista.ti@gmail.com>
2021-10-23 11:57:24 -0300

    test

--- pages/signup/__test__/signup.test.tsx
+++ pages/signup/__test__/signup.test.tsx
@@ -1,5 +1,5 @@
 import {} from '@testing-library/react'
 
 test('Should render a FormSelect component', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
 })
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <div>Vai ser aqui o cadstro</div>
+  return <div>Vai ser aqui o cadastro teste</div>
 }
 
 export default Home
